44ce87007dccb98e4b3d92070f31cf591eb01cfe
Youri Daamen <youri@vigour.io>
2014-06-24 18:53:42 +0200

    fixed server gaston

--- webserver.js
+++ webserver.js
@@ -3,14 +3,16 @@ var connect = require('connect')
 	,	fs = require('fs')
 	, app = connect()
 	, log = require('npmlog')
+	, path = require('path')
 
 module.exports = function( folder, port ){
 	app.use(function(req, res){
-		fs.readFile(process.cwd() + (req.url || '/index.html'), function(err, data){
-			if (err) log.error('server', err)
-				res.end(data);
+		var request = path.join(process.cwd(), req.url)
+		if(!path.extname(request)) request += '/index.html'
+		fs.readFile(request, function(err, data){
+			if (err) log.error('server',request) 
+				res.end(data)
 		})
-	  
 	})
 	http.createServer(app).listen(port || 8080)
 
