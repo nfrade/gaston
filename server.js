e9bc3c9b5b8a8060d745207b49e6bd875706fc6b
Marcus Besjes <marcusbesjes@gmail.com>
2014-07-21 17:27:09 +0200

    make server ignore uri variables ? #

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
+module.exports = startServer
+
 var fs = require('graceful-fs')  
   , http = require('http')
+  , url = require('url')
   , path = require('path')
   , log = require('npmlog')
 
-module.exports = function(port,dir){
+function startServer(port){
+  var server = http.createServer(function (req, res) {
+    if(~req.url.indexOf('__retry')) {
+      log.info('retry')
+      require('./gaston')()
+    } else {
+      reader(getTarget(req.url), res)
+    }
+  })
+  log.info('start server on port: ', port)
+  server.listen(port)  
+}
 
-  var reader = function(url, res, cnt) {
- 
-      fs.exists(url, function(exists) {
-        if(!exists) { 
-          if(!cnt) {
-            reader(url,res, (cnt=true) )
-          } else {
-            var urlpath = path.join(process.cwd(),url)
-            log.error('cannot find file', urlpath)
-            res.end('file does not exist! ' + urlpath)
-          }
+function reader(url, res, cnt) {
+  fs.exists(url, function(exists) {
+    if(!exists) { 
+      if(!cnt) {
+        reader(url,res, (cnt=true) )
+      } else {
+        var urlpath = path.join(process.cwd(),url)
+        log.error('cannot find file', urlpath)
+        res.end('file does not exist! ' + urlpath)
+      }
+    } else {
+      fs.stat(url,function(err,stats) {
+        if(stats.isDirectory()) {
+          if(url[0]==='/') url = url.slice(1)
+          reader(path.join(url,'index.html'),res)
         } else {
-          fs.stat(url,function(err,stats) {
-            if(stats.isDirectory()) {
-              if(url[0]==='/') url = url.slice(1)
-              reader(path.join(url,'index.html'),res)
-            } else {
-              var rstream = fs.createReadStream(url)
-              rstream.pipe(res)
-            }
-          })
+          var rstream = fs.createReadStream(url)
+          rstream.pipe(res)
         }
       })
     }
+  })
+}
 
-  , server = http.createServer(function (req, res) {
-      if(~req.url.indexOf('__retry')) {
-        log.info('retry')
-        require('./gaston')()
-      }else reader(req.url.slice(1) || 'index.html',res)
-    })
-
-  log.info('start server on port: ',port)
-  server.listen(port)  
+function getTarget(str){
+  return url.parse(str.slice(1)).pathname || 'index.html'
 }
