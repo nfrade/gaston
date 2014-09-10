813ef46d0e41f9cce4a7c5ec2dde0c7719d6bb24
Marcus Besjes <marcusbesjes@gmail.com>
2014-09-10 19:17:49 +0200

    fixed imports

--- compile.js
+++ compile.js
@@ -8,36 +8,66 @@ var watchify = require('watchify')
 var watchifies = {}
 var rebase = require('./rebase.js')
 
-module.exports = function(entry, opts, cb) {
-  var index = entry || './index.js'
-  var w = watchifies[index] 
+module.exports = function(entry, opts, callback) {
+  var w = watchifies[entry]
+  if(!w) w = createWatchify(entry,opts)
+  if(!w._compiling) callback(null,watchifies)
+  w._callback = callback
+}
 
-  if(!w){
-    var basedir = path.dirname(entry)
-    var bundleOptions = watchify.args   
-    
-    if(opts){
-      bundleOptions.debug = opts.debug
-      bundleOptions.ignoreMissing = opts.ignoreMissing
-    }
-
-    var b = browserify(index,bundleOptions)
-    w = watchifies[index] = watchify(b)
-
-    w.transform({global:opts && opts.global || true},function(file){ return handleDeps(w,file) })
-    w._basedir = basedir
-
-    w.on('log', function (msg) { log.info('watchify',msg) })
-    w.on('update',compile)
-    w.on('dep',perhapsCompileCSS)
-    w.on('done',fileDone)
-
-    compile.call(w)
+function createWatchify(entry,opts){
+  var basedir = path.dirname(entry)
+  var bundleOptions = watchify.args   
+  
+  if(opts){
+    bundleOptions.debug = opts.debug
+    bundleOptions.ignoreMissing = opts.ignoreMissing
+    bundleOptions.noParse = opts.noParse
   }
 
-  w._callback = cb
+  var b = browserify(entry,bundleOptions)
+  var w = watchifies[entry] = watchify(b)
 
-  if(!w._compiling) cb(null,watchifies)
+  var transformOptions = {global:opts && opts.global || true}
+
+  w.transform(transformOptions,handleDeps.bind(w))
+  w._basedir = basedir
+
+  w.on('log',log.info)
+  w.on('update',compile)
+  w.on('dep',perhapsCompileCSS)
+  w.on('done',complete)
+
+  compile.call(w)
+
+  return w
+}
+
+function handleDeps(file){
+  var w = this
+  var todo
+
+  if( isCSS(file) ){
+    todo = function(){ this.push(null) }
+
+    if(!w._cssdeps) w._cssdeps = {}
+    if(!w._cssprocessing) w._cssprocessing = 1
+    else w._cssprocessing++
+
+    fs.readFile(file, 'utf8', function(err,data){
+      if(err) w._callback(err)
+      rebaseCSS(w,file,data)
+    })
+  }
+
+  return through(todo)
+}
+
+function rebaseCSS(w,file,data){
+  var base = path.relative(w._basedir, path.dirname(file))
+  if (base.length) base += '/'
+  w._cssdeps[file] = rebase(data,base)
+  if(!--w._cssprocessing && w._depscomplete) compileCSS(w)
 }
 
 function compile(){
@@ -58,26 +88,9 @@ function compile(){
       }))
 }
 
-function perhapsCompileCSS(dep){
-  var processing = this._cssprocessing
-  if(!processing){
-    if(updatedCSS(this) || processing === 0){
-      compileCSS(this)
-    }
-    this._cssprocessing = true //complete
-  }
-  this._depscomplete = true
-}
-
-function fileDone(done){
-  if(done === 'css'){
-    log.info('css bundle complete')
-    this._csscomplete = true
-  }
-  if(done === 'js'){
-    log.info('js bundle complete')
-    this._jscomplete = true
-  }
+function complete(done){
+  if(done === 'css') this._csscomplete = true
+  if(done === 'js') this._jscomplete = true
 
   var everythingDone = this._csscomplete && this._jscomplete
 
@@ -85,23 +98,42 @@ function fileDone(done){
     this._compiling = false
     if(this._callback) this._callback(null,watchifies) 
   }
+
+  log.info(done + ' bundle complete')
 }
 
-//compiles css/less to final bundle.css
+
+function perhapsCompileCSS(dep){
+  var processing = this._cssprocessing
+
+  if(!processing){
+    if(updatedCSS(this) || processing === 0) compileCSS(this)
+    this._cssprocessing = true
+  }
+
+  this._depscomplete = true
+}
+
 function compileCSS(w){
-  w._csscomplete = false
   var css = concatCSS(w)
   var output = path.join(w._basedir,'bundle.css')
+
+  w._csscomplete = false
+
   less.Parser({paths:[w._basedir]}).parse(css, function (err, tree) {
     if(err) w._callback(err)
     var rules = tree.rules
-    for (var i = rules.length - 1; i >= 0; i--) {
+    var i = rules.length - 1
+    var imports = []
+
+    for (; i >= 0; i--) {
       var importedFilename = rules[i].importedFilename
-      if(importedFilename && !~w._cssarr.indexOf(importedFilename)){
+      if(importedFilename && !~imports.indexOf(importedFilename)){
         w.emit('file',importedFilename)
-        w._cssarr.push(importedFilename)
+        imports.push(importedFilename)
       }
     }
+
     try { 
       fs.writeFile(output,tree.toCSS(),function(err){
         if(err) w._callback(err)
@@ -112,7 +144,6 @@ function compileCSS(w){
   })
 }
 
-//concatenates css
 function concatCSS(w){
   var cssarr = w._cssarr || updatedCSS(w)
   var merge = ''
@@ -124,7 +155,6 @@ function concatCSS(w){
   return merge
 }
 
-//check if there is a change in css dependencies
 function updatedCSS(w){
   var arr = []
   var prevarr = w._cssarr
@@ -132,42 +162,21 @@ function updatedCSS(w){
     if( isCSS(file) ) arr.push(file)
   }
   var theSame = prevarr && compareArray(prevarr,arr)
+  console.log(!theSame,arr.length)
+
   if(!theSame) return w._cssarr = arr
 }
 
-//compareArrayContents only for contents => NOT order
 function compareArray(a,b){
-  if(a.length == b.length){
-    for (var i = a.length - 1; i >= 0; i--) {
+  var alength = a.length
+  if(alength == b.length){
+    for (var i = alength - 1; i >= 0; i--) {
       if(!~b.indexOf(a[i])) return false
     }
     return true
   }
 }
 
-//checks if file is css or less
 function isCSS(file){
   if(~file.indexOf('.less') || ~file.indexOf('.css')) return true
-}
-
-//handles deps, rejecting css and less, sending them to css parser
-function handleDeps(w,file){
-  if( isCSS(file) ){
-    if(!w._cssdeps) w._cssdeps = {}
-    w._cssprocessing = (w._cssprocessing || 0) + 1
-    fs.readFile(file, 'utf8', function(err,data){
-      if(err) w._callback(err)
-      rebaseCSS(w,file,data)
-    })
-    return through(function(){this.push(null)})
-  }
-  return through()
-}
-
-//rebases css
-function rebaseCSS(w,file, data){
-  var base = path.relative(w._basedir, path.dirname(file))
-  if (base.length) base += '/'
-  w._cssdeps[file] = rebase(data,base)
-  if(!--w._cssprocessing && w._depscomplete) compileCSS(w)
 }
