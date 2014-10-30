8e19ee42f5512adfb3902083081de3c96c1b6685
Marcus Besjes <marcusbesjes@gmail.com>
2014-10-30 11:04:31 +0100

    ignoremissing default to true

--- compile.js
+++ compile.js
@@ -24,7 +24,9 @@ function createWatchify(entry,opts){
   
   if(opts){
     bundleOptions.debug = opts.debug
-    bundleOptions.ignoreMissing = opts.ignoreMissing
+    bundleOptions.ignoreMissing = opts.ignoreMissing !== void 0 
+      ? opts.ignoreMissing
+      : true
     bundleOptions.noParse = opts.noParse
   }
 
