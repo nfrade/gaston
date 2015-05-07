1534e4580903852b04ec6d10ebb259f151cf6cea
André Padez <andre.padez@gmail.com>
2015-05-07 16:14:37 +0200

    added option to bypass injecting package.json

--- bin/gaston-dev.js
+++ bin/gaston-dev.js
@@ -2,20 +2,23 @@ var program = require('commander')
   , gaston = require('../lib');
 
 program
+  .version('0.0.1')
+  .option( '-i, --no-inject-package ', 'don\'t inject package.json')
   .option( '-p, --port [port]', 'run server on port <port> ( default 8080 )', parseInt )
   .option( '-P, --path [path]', 'run server from path <path> ( default process.cwd() )' )
-  .option( '-r, --reload [reload], auto-reload on recompiling')
+  .option( '-r, --reload [reload]', 'auto-reload on recompiling')
   .parse(process.argv);
 
 var options = {
   port: program.port || 8080,
   path: program.path || process.cwd(),
-  autoreload: program.reload === 'false'? false : true
-}
+  autoreload: program.reload === 'false'? false : true,
+  injectPackage: program.injectPackage
+};
 
 gaston.dev(options)
 
-process.on('SIGINT', function() {  
+process.on('SIGINT', function() {
   console.log('\nGaston, out!');
   process.exit(0);
 });
