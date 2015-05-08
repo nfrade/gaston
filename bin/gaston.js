9d7f5111de56f6adc5ecb12fc584833e87acea8d
André Padez <andre.padez@gmail.com>
2015-05-08 18:03:21 +0200

    some sugar

--- bin/gaston.js
+++ bin/gaston.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
-var program = require('commander');
+var program = require('commander'),
+  pkg = require('../package.json');
 
 if (process.argv.indexOf('dev') === -1 && process.argv.indexOf('build') === -1) {
   process.argv.push('dev');
 }
 
 program
-  .version('0.5.0')
+  .version(pkg.version)
   .command('dev', 'run development environment')
   .command('build', 'build target')
   .parse(process.argv);
