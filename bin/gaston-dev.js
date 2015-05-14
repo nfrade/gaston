feb4654600f66bb7c1c5cb6b8cdf4107142c8d0f
André Padez <andre.padez@gmail.com>
2015-05-14 13:05:34 +0200

    added lazy-start option

--- bin/gaston-dev.js
+++ bin/gaston-dev.js
@@ -3,6 +3,7 @@ var program = require('commander')
 
 program
   .option( '-i, --no-inject-package ', 'don\'t inject package.json')
+  .option( '-l, --lazy-start', 'don\'t launch or reload page when starting')
   .option( '-p, --port [port]', 'run server on port <port> ( default 8080 )', parseInt )
   .option( '-P, --path [path]', 'run server from path <path> ( default process.cwd() )' )
   .option( '-r, --reload [reload]', 'auto-reload on recompiling')
@@ -12,7 +13,8 @@ var options = {
   port: program.port || 8080,
   path: program.path || process.cwd() + '/',
   autoreload: program.reload === 'false'? false : true,
-  injectPackage: program.injectPackage
+  injectPackage: program.injectPackage,
+  lazyStart: program.lazyStart
 };
 
 gaston.dev(options)
