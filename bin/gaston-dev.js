d7e008f7f57e387ffc7a1d7bc2c765c2be842a16
André Padez <andre.padez@gmail.com>
2015-05-17 21:18:47 +0200

    major config refactor

--- bin/gaston-dev.js
+++ bin/gaston-dev.js
@@ -5,7 +5,6 @@ program
   .option( '-i, --no-inject-package ', 'don\'t inject package.json')
   .option( '-l, --lazy-start', 'don\'t launch or reload page when starting')
   .option( '-p, --port [port]', 'run server on port <port> ( default 8080 )', parseInt )
-  .option( '-P, --path [path]', 'run server from path <path> ( default process.cwd() )' )
   .option( '-r, --reload [reload]', 'auto-reload on recompiling')
   .parse(process.argv);
 
--- bin/gaston.js
+++ bin/gaston.js
@@ -1,14 +1,2 @@
 #!/usr/bin/env node
-var program = require('commander'),
-  pkg = require('../package.json');
-
-if (process.argv.indexOf('dev') === -1 && process.argv.indexOf('build') === -1) {
-  process.argv.push('dev');
-}
-
-program
-  .version(pkg.version)
-  .command('init', 'bootstrap a project')
-  .command('dev', 'run development environment')
-  .command('build', 'build target')
-  .parse(process.argv);
+require('../lib/gaston.js')
