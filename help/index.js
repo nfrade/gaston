461bf681ccdb2b501d2ba4f8c13c9bbcdcf2d5b8
André Padez <andre.padez@gmail.com>
2015-05-19 15:14:05 +0200

    added command line help

---
+++ help/index.js
@@ -0,0 +1,20 @@
+var log = require('npmlog')
+  , fs = require('graceful-fs')
+  , path = require('path')
+  , topic = process.argv[3];
+
+if(!topic){
+  fs.createReadStream( path.join(__dirname, 'help.txt') )
+    .pipe(process.stdout);
+} else {
+  var topicPath = path.join(__dirname, topic + '.txt' );
+  fs.exists(topicPath, function(exists){
+    if(exists){
+      fs.createReadStream( topicPath )
+        .pipe(process.stdout);
+    } else {
+      log.error('help', 'no command named ' + topic + ';');
+      log.info('help', 'type "gaston help" for a list of commands');
+    }
+  });
+}
