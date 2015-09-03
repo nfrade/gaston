c067260d819e58922a46e742c0bd20b2f32437ea
Danillo Corvalan <danillo.corvalan@gmail.com>
2015-09-03 11:41:28 -0300

    Improving uncaught and unhandled exceptions/rejections #63

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,18 @@
 var tester = require('../lib/tester')
 var parseArgs = require('minimist');
 var args = parseArgs(process.argv);
+var log = require('npmlog');
+
+
+process.on('unhandledRejection', function (err) {
+  throw err;
+});
+
+process.on('uncaughtException', function (err) {
+  log.error(config.command, err.stack);
+  process.exit(1);
+});
+
 
 module.exports = function(cfg){
   config = cfg;
