62e9c70297474a914c98ceab227a99e3a2f2dd43
André Padez <andre.padez@gmail.com>
2015-09-08 15:06:11 +0200

    moved to lib

--- bin/gastond.js
+++ bin/gastond.js
@@ -1,28 +1,2 @@
 #!/usr/bin/env node
-var log = require('npmlog')
-  , fs = require('vigour-fs-promised')
-  , path = require('path')
-  , api = require('../daemon/api')
-  , config = require('../config.json')
-  , infoDescriptor
-  , errorDescriptor;
-
-fs.openAsync( path.join(__dirname, '..', 'logs', 'info.log'), 'w' )
-  .then(function(fd){
-    infoDescriptor = fd;
-    return fs.openAsync( path.join(__dirname, '..', 'logs', 'error.log'), 'w' )
-  })
-  .then(function(fd){
-    errorDescriptor = fd;
-  })
-  .then(function(){
-    // require('daemon')({
-    //   stdout: infoDescriptor,
-    //   stderr: errorDescriptor 
-    // });
-    
-    require('../daemon');
-  });
-
-log.info('gaston', 'is now running as a daemon');
-
+require('../lib/daemon');
