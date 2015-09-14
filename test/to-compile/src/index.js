796f6f59891ba0525bedcb83590db56cbea45b4e
André Padez <andre.padez@gmail.com>
2015-09-14 17:36:04 +0200

    middleware is working

--- test/to-compile/src/index.js
+++ test/to-compile/src/index.js
@@ -5,3 +5,9 @@ require('./module');
 var Base = require('vjs/lib/base')
 
 console.log('in src/index.js');
+
+var a = new Base({
+  b: 'bbbbbbbbb'
+});
+
+console.log('a', a);
--- test/to-compile/src/module/index.js
+++ test/to-compile/src/module/index.js
@@ -1,3 +1,5 @@
 require('./styles.less')
 
 console.log('in src/module/index.js');
+
+console.log('hehehehehehe')
