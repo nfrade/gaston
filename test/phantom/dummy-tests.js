9d09ebfc01a6f41f3bc6efd9d468660da16b71d6
André Padez <andre.padez@gmail.com>
2015-09-18 20:35:33 +0200

    cli common tests running

--- test/phantom/dummy-tests.js
+++ test/phantom/dummy-tests.js
@@ -1,6 +1,14 @@
 describe('some tests to see if the tester is working', function(){
 
   it('should do some stuff', function(){
+    assert.equal(1, 0);
+  });
+
+  it('should do some stuff', function(){
+    assert.equal(1, 0);
+  });
+
+  it('should do some stuff', function(){
     assert.equal(1, 1);
   });
 
