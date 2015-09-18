51b2c7b0b2add7d4b5f5d76c461c72da647bfa4c
André Padez <andre.padez@gmail.com>
2015-09-18 18:05:18 +0200

    cli tests for node working

---
+++ test/node/dummy-tests.js
@@ -0,0 +1,15 @@
+describe('some tests to see if the tester is working', function(){
+
+  it('should do some stuff', function(){
+    assert.equal(1, 0);
+  });
+
+  it('should do some stuff', function(){
+    assert.equal(1, 0);
+  });
+
+  it('should do some stuff', function(){
+    assert.equal(1, 1);
+  });
+
+});
