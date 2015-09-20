b73079adb4683d83cf4022ec50a0f8c563b4191f
André Padez <andre.padez@gmail.com>
2015-09-20 19:14:12 +0200

    refactored config, and now is ~/.gaston/config.json

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -3,12 +3,19 @@ var os = require('os')
   , path = require('path')
   , fs = require('vigour-fs-promised')
   , homedir = process.env.USERPROFILE || process.env.HOME
-  , configPath = path.join(homedir, '.gaston', 'config/gaston.json')
+  , configPath = path.join(homedir, '.gaston', 'config.json')
   , initialConfig = {
-    "http-port": 8080,
-    "api-port": 64571,
-    "base-path": process.env.HOME || process.env.USERPROFILE
-  };
+      "http-port": 8080,
+      "api-port": 64571,
+      "base-path": process.env.HOME || process.env.USERPROFILE,
+      "source-maps": false,
+      "smaps": true,
+
+      "push-state": false,
+      "index-path": false,
+      "css-compiler": "less",
+      "aliases": {}
+    };
 
 fs.existsAsync( configPath )
   .then(function(exists){
