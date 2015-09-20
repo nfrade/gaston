bf47901b52230f0a3aa86665ea97f2e6f28ab615
André Padez <andre.padez@gmail.com>
2015-09-20 23:20:15 +0200

    gaston-config working, misses validation

--- bin/gaston-config.js
+++ bin/gaston-config.js
@@ -1,22 +1,57 @@
-var fs = require('fs');
-var child_process = require('child_process');
-var spawn = child_process.spawn;
+#!/usr/bin/env node
+var log = require('npmlog')
+  , fs = require('vigour-fs-promised')
+  , path = require('path')
+  , child_process = require('child_process')
+  , spawn = child_process.spawn
+  , Config = global.Config = require('../lib/config')
+  , client = require('../lib/io-client')
+  , homedir = process.env.USERPROFILE || process.env.HOME
+  , configPath = path.join(homedir, '.gaston', 'config.json')
+  , config;
 
+Config.init()
+  .then(function(){
+    return client.connect();
+  })
+  .then( makeBackup )
+  .then(function(){
+    return openEditor(configPath);
+  })
+  .then(function(content){
+    console.log('content', content);
+  })
+  // .then(validateNewContent)
+  .then(function(){
+    process.exit(0);
+  })
+  .catch(function(){
+    log.error('gaston', 'cannot connect to daemon');
+    process.exit(1);
+  });
 
+function makeBackup(){
+  return new Promise(function(resolve, reject){
+    var rs = fs.createReadStream(configPath);
+    var ws = fs.createWriteStream(configPath + '.backup');
+    ws.on('close', resolve);
+    rs.pipe( ws );
+  });
+}
 
 
 function openEditor(file) {
-  var cp = spawn(process.env.EDITOR, [file], {
-    customFds: [
-      process.stdin,
-      process.stdout,
-      process.stderr
-    ]
+  return new Promise(function(resolve, reject){
+    var cp = spawn(process.env.EDITOR || 'vim', [file], {
+      stdio: [
+        process.stdin,
+        process.stdout,
+        process.stderr
+      ]
+    });
+    cp.on('exit', function() {
+      fs.readFileAsync(file, 'utf8')
+        .then(resolve);
+    });
   });
-  cp.on('exit', function() {
-    console.log('editor ended');
-    var content = fs.readFileSync(file, 'utf8');
-    console.log(content);
-  });
-
 }
