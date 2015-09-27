ed5006d090bf73cd5d8c44ea8e988e5aea674156
André Padez <andre.padez@gmail.com>
2015-09-28 00:30:11 +0200

    browser tests are working

--- test/phantom/dummy-tests.js
+++ test/phantom/dummy-tests.js
@@ -1,15 +1,13 @@
-describe('some tests to see if the tester is working', function(){
+describe('some tests to see if the tester is working', function () {
+  it('should do some stuff', function () {
+    assert.equal(1, 1)
+  })
 
-  it('should do some stuff', function(){
-    assert.equal(1, 0);
-  });
+  it('should do some stuff', function () {
+    assert.equal(1, 1)
+  })
 
-  it('should do some stuff', function(){
-    assert.equal(1, 0);
-  });
-
-  it('should do some stuff', function(){
-    assert.equal(1, 1);
-  });
-
-});
+  it('should do some stuff', function () {
+    assert.equal(1, 1)
+  })
+})
