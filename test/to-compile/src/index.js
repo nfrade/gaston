21c812e341925674f9b94c4a6b5f8dfef1fa2b46
André Padez <andre.padez@gmail.com>
2015-10-24 10:19:09 +0200

    started gaston start process

--- test/to-compile/src/index.js
+++ test/to-compile/src/index.js
@@ -2,12 +2,4 @@ require('./styles.less')
 
 require('./module')
 
-var Base = require('vjs/lib/base')
-
 console.log('in src/index.js')
-
-var a = new Base({
-  b: 'bbbbbbbbb'
-})
-
-console.log('a', a)
