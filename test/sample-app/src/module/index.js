4d3febb184f3adf6a47221ff6aa907ec7cee2606
André Padez <andre.padez@gmail.com>
2015-11-26 17:43:45 +0100

    renamed to-compile to sample-app

---
+++ test/sample-app/src/module/index.js
@@ -0,0 +1,8 @@
+require('./styles.less')
+
+console.log('in src/module/index.js')
+
+console.log('hehehehehehe')
+
+require('gaston/test/sample-app/src/module/some-file')
+console.log('required gaston/test/sample-app/src/module/some-file')
